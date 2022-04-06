0e2eb933cdeb672d22366b447e3e80f62e93ba99
Abhinav_Tyagi <nonityagi@gmail.com>
2022-04-06 14:28:55 +0530

    Added first formula to calculate exponent

---
+++ formulas.js
@@ -0,0 +1,6 @@
+// Function to find exponent
+function power(){
+  n = document.getElementById('input1').value
+  d = document.getElementById("input2").value
+  document.getElementById("answer").innerHTML=n**b
+}
