9ef9f842f4b7814e7e66c063d79143967580e87f
Abhinav_Tyagi <nonityagi@gmail.com>
2022-04-06 15:16:51 +0530

    Added functions in formulas.js

--- js/formulas.js
+++ js/formulas.js
@@ -76,3 +76,40 @@ function primechecker(){
     document.getElementById("answer").innerHTML=n+" is not a Prime Number."
   }
 }
+
+// Function to find arc sin in radian
+function arcsin(){
+  n = document.getElementById("input1").value
+  answer = Math.asin(n)
+  document.getElementById("answer").innerHTML=answer
+}
+
+// Function to find arc cos in radian
+function arccos(){
+  n = document.getElementById("input1").value
+  answer = Math.acos(n)
+  document.getElementById("answer").innerHTML=answer
+}
+
+// Function to find arc tan in radian
+function arctan(){
+  n = document.getElementById("input1").value
+  answer = Math.atan(n)
+  document.getElementById("answer").innerHTML=answer
+}
+
+//function to find determinant of a 3X3 Matrix
+function determinant(){
+  a=document.getElementById("input1").value
+  b=document.getElementById("input2").value
+  c=document.getElementById("input3").value
+  d=document.getElementById("input4").value
+  f=document.getElementById("input5").value
+  e=document.getElementById("input6").value
+  g=document.getElementById("input7").value
+  h=document.getElementById("input8").value
+  i=document.getElementById("input9").value
+
+  answer = a*(e*i-f*h)-b*(d*i-f*g)+c*(d*h-e*g);
+  document.getElementById("answer").innerHTML=answer;
+}
